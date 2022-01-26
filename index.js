9f29043d4373cc4a8249a26e8b15d3acf110d09e
Alissa <alissamak@outlook.com>
2022-01-26 18:52:46 -0500

    added DOM elements in javascript

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Select some elements...
+let header = document.querySelector('#page-header')
+header.style.textAlign = 'left'
+
+let dogImages = document.querySelectorAll('.dog-image')
+
+for (let i=0; i < dogImages.length; i++){
+    dogImages[i].style.borderRadius = '50px'
+    //dogImages[i].style.transform = 'rotateX(180deg)'
+    dogImages[i].style.transform = 'scale(-1,-1)'
+}
+
+let dogNames = document.querySelectorAll('.dog-name')
+for (let i=0; i < dogNames.length; i++){
+    dogNames[i].style.textAlign = 'left'
+}
+
+let footer = document.querySelector('.footer')
+footer.style.color = 'purple'
+
